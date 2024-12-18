57efd30a63079566b24d42f972ecaeb8353d7d9d
astonxy <victor10macielrodrigues@gmail.com>
2024-12-18 18:18:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+        e.preventDefault();
+        alert('Por favor, preencha todos os campos!');
+    }
+});
